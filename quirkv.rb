5fd7ba8ab10a0b511deeb7a6eb0f9d7bafa55a31
Brian Muckian <bmuckian@uw.edu>
2013-05-25 09:58:11 -0700

    list data sources (from config file), list queries (which is the first query from the first data source...oh boy).  Next let's get some new/edit forms

--- quirkv.rb
+++ quirkv.rb
@@ -1,13 +1,46 @@
+#!/usr/bin/env ruby
+require 'rubygems'
 require 'sinatra'
+require 'stringio'
 require 'haml'
 require 'json'
+require 'sqlite3'
+
+helpers do
+  def data_sources()
+    JSON.parse(IO.read("config/data_sources.json"))
+  end
+
+  def get_query(qid)
+    q = "select * from queries where qid = '#{qid}';"
+    qdb = data_sources.first{|e| e["name"] == "queries"}
+    case qdb["type"]
+      when "sqllite"
+        db = SQLite3::Database.open "#{qdb["parameters"]["location"]}"
+        results = db.execute(q)
+      when "mssql"
+        # do something else...
+      else
+        # fix your config file?
+      end
+    results
+  end
+  
+end
 
 # / index, present user with "new data source, query" and list data sources, queries
 get '/' do
   # get data sources from config file
-  @data_sources = JSON.parse(IO.read("config/data_sources.json"))
-  
+  @data_sources = data_sources
+
   # get queries from queries database
+  # http://stackoverflow.com/questions/3551746/calling-sinatra-from-within-sinatra
+  @queries = eval(self.call(
+    'REQUEST_METHOD' => 'GET',
+    'PATH_INFO' => '/e/YVk4I7U',
+    'rack.input' => StringIO.new
+  )[2].join(''))
+
   haml :index
 end
 
@@ -25,9 +58,12 @@ end
 
 # /e/:qid execute query return data (CSV, JSON)
 get '/e/:qid' do
-  
+  @results = get_query("#{params[:qid]}").to_s
+  # execute_query(q)
+  # connect to the queries database, get the dsname and qtext given the qid
+  # run the query, deliver the results
+  @results
 end  
-
 
 __END__
 
@@ -37,4 +73,15 @@ __END__
   %h3 data sources
   - @data_sources.each do |e|
     %a{:href => "d/#{e["name"]}"}= e["name"]
-  
+  %h3 queries
+  - @queries.each do |e|
+    %a{:href => "q/#{e[0]}"}= e[2]
+ 
+@@ dform
+%html
+  %h2 data source form
+ 
+@@ qform
+%html
+  %h2 query form
+    
